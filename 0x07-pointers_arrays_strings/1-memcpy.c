ca8811d6fc09ddb2010f7045eeacc971420792f7
Fomonyuy-Claire <fomonyuymarie2000@gmail.com>
2023-08-29 17:48:58 -0700

    C - Even more pointers, arrays and strings all task completed

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	int i = 0;
 	int m = n;
 
-	for (; i < m; r++)
+	for (; i < m; m++)
 	{
 		dest[i] = src[i];
 		n--;
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,7 +11,7 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-		for (m = 0; m < 8; j++)
+		for (m = 0; m < 8; m++)
 			_putchar(a[i][m]);
 		_putchar('\n');
 	}
